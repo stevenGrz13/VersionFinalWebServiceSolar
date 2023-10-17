7a63c12eba35dce172a65c27a88482076c0546a8
stevenGrz13 <razafimahefasteven130102@gmail.com>
2023-10-17 23:20:27 +0200

    ---
    ----------

--- demo5/src/main/java/com/example/demo5/solar/entities/Donnee.java
+++ demo5/src/main/java/com/example/demo5/solar/entities/Donnee.java
@@ -3,59 +3,100 @@ package com.example.demo5.solar.entities;
 import java.sql.Timestamp;
 
 public class Donnee {
+    int Id;
 
+    int Idmodule;
+    double VoltagePanneau;
+    double VoltageOutPut;
+    double Consommation;
+    double Production;
+
+    double ValeurBatterie;
+
+    double VoltageBatterie;
+    Timestamp Temps;
     public Donnee(){}
-    public Donnee(double voltage, double courant, double energie, double puissance, Timestamp temps) {
-        this.voltage = voltage;
-        this.courant = courant;
-        this.energie = energie;
-        this.puissance = puissance;
-        this.temps = temps;
+
+    public Donnee(int idmodule, double voltagePanneau, double voltageOutPut, double consommation, double production, double valeurBatterie, double voltageBatterie, Timestamp temps) {
+        Idmodule = idmodule;
+        VoltagePanneau = voltagePanneau;
+        VoltageOutPut = voltageOutPut;
+        Consommation = consommation;
+        Production = production;
+        ValeurBatterie = valeurBatterie;
+        VoltageBatterie = voltageBatterie;
+        Temps = temps;
     }
 
-    double voltage;
-    double courant;
-    double energie;
-    double puissance;
-    Timestamp temps;
-
-    public double getVoltage() {
-        return voltage;
+    public int getIdmodule() {
+        return Idmodule;
     }
 
-    public void setVoltage(double voltage) {
-        this.voltage = voltage;
+    public void setIdmodule(int idmodule) {
+        Idmodule = idmodule;
     }
 
-    public double getCourant() {
-        return courant;
+    public double getVoltageBatterie() {
+        return VoltageBatterie;
     }
 
-    public void setCourant(double courant) {
-        this.courant = courant;
+    public void setVoltageBatterie(double voltageBatterie) {
+        VoltageBatterie = voltageBatterie;
     }
 
-    public double getEnergie() {
-        return energie;
+    public int getId() {
+        return Id;
     }
 
-    public void setEnergie(double energie) {
-        this.energie = energie;
+    public void setId(int id) {
+        Id = id;
     }
 
-    public double getPuissance() {
-        return puissance;
+    public double getVoltagePanneau() {
+        return VoltagePanneau;
     }
 
-    public void setPuissance(double puissance) {
-        this.puissance = puissance;
+    public void setVoltagePanneau(double voltagePanneau) {
+        VoltagePanneau = voltagePanneau;
+    }
+
+    public double getVoltageOutPut() {
+        return VoltageOutPut;
+    }
+
+    public void setVoltageOutPut(double voltageOutPut) {
+        VoltageOutPut = voltageOutPut;
+    }
+
+    public double getConsommation() {
+        return Consommation;
+    }
+
+    public void setConsommation(double consommation) {
+        Consommation = consommation;
+    }
+
+    public double getProduction() {
+        return Production;
+    }
+
+    public void setProduction(double production) {
+        Production = production;
+    }
+
+    public double getValeurBatterie() {
+        return ValeurBatterie;
+    }
+
+    public void setValeurBatterie(double valeurBatterie) {
+        ValeurBatterie = valeurBatterie;
     }
 
     public Timestamp getTemps() {
-        return temps;
+        return Temps;
     }
 
     public void setTemps(Timestamp temps) {
-        this.temps = temps;
+        Temps = temps;
     }
 }
